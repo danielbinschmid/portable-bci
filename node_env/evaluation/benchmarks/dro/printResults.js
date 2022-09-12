7d30b6fa2cd030e4e4c9010fc40bfb7a3a0bb228
Daniel <st161382@stud.uni-stuttgart.de>
2022-09-12 19:02:07 +0200

    -

--- node_env/evaluation/benchmarks/dro/printResults.js
+++ node_env/evaluation/benchmarks/dro/printResults.js
@@ -49,7 +49,18 @@ function crossSubjectCrossSessionAccs() {
         }
     }
 
-    console.log(accs);
+    const accs_ = {}
+    accs_["Test accuracy after DRO"] = accs["testAccBefore"] 
+    accs_["Test accuracy before DRO"] = accs["finetunedTestingAcc"] 
+    accs_["Training accuracy after DRO"] = accs["trainingAccBefore"] 
+    accs_["Training accuracy before DRO"] = accs["finetunedTrainingAcc"] 
+
+
+    console.log("------ CROSS-SESSION EVALUATION ------")
+    console.log("Network: Riemann-CiM-HRR with DRO")
+    
+    console.log(accs_)
+    console.log("^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^")
 }
 
 function overfitReduce() {
@@ -75,13 +86,7 @@ function overfitReduce() {
     ]
 
     const accs = {}
-    for (const subj of subjects) {
-        accs[subj] = {}
-        accs[subj]["trainAccBefore"] = 0;
-        accs[subj]["testAccBefore"] = 0;
-        accs[subj]["trainAccAfter"] = 0;
-        accs[subj]["testAccAfter"] = 0;
-    }
+
     accs["trainAccBefore"] = 0;
     accs["testAccBefore"] = 0;
     accs["trainAccAfter"] = 0;
@@ -102,18 +107,16 @@ function overfitReduce() {
                     accs["testAccBefore"] += testAcc / (runs.length * sessions.length * subjects.length);
                     accs["trainAccAfter"] += trainAccAfter / (runs.length * sessions.length * subjects.length);
                     accs["testAccAfter"] += testAccAfter / (runs.length * sessions.length * subjects.length);
-
-                    accs[subj]["trainAccBefore"] += trainAcc / (runs.length * sessions.length);
-                    accs[subj]["testAccBefore"] += testAcc / (runs.length * sessions.length);
-                    accs[subj]["trainAccAfter"] += trainAccAfter / (runs.length * sessions.length);
-                    accs[subj]["testAccAfter"] += testAccAfter / (runs.length * sessions.length);
-
                 }
             }
         }
     }
 
-    console.log(accs);
+    console.log("------ TRAINING/TEST ACC W/O DRO ------")
+    console.log("Network: Riemann-CiM-HRR with DRO")
+    
+    console.log(accs)
+    console.log("^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^")
 }
 
 
@@ -140,7 +143,7 @@ function crossSubjectCrossSessionAccsPercentiles() {
     ]
 
     const accs = {}
-    accs["acc_before"] = {}
+    accs["acc_before"] = 0
     const accsBefore = {}
     for (const id of percentiles) {
         accs[percentile_prefix + id] = 0;
@@ -153,13 +156,17 @@ function crossSubjectCrossSessionAccsPercentiles() {
             for (const session of sessions) {
                 for (const percentile of percentiles) {
                     accs[percentile_prefix + percentile] += d1[run_prefix + run][session][subj_prefix + subj][percentile_prefix + percentile]["finetunedTestingAcc"] / (runs.length * sessions.length * subjects.length);
-                    accs["acc_before"][percentile_prefix + percentile] += d1[run_prefix + run][session][subj_prefix + subj][percentile_prefix + percentile]["testAccBefore"] / (runs.length * sessions.length * subjects.length);
+                    accs["acc_before"] += d1[run_prefix + run][session][subj_prefix + subj][percentile_prefix + percentile]["testAccBefore"] / (runs.length * sessions.length * subjects.length * percentiles.length);
                 }
             }
         }
     }
 
-    console.log(accs);
+    console.log("------ DRO HYPERPARAMETER ------")
+    console.log("Network: Riemann-CiM-HRR with DRO")
+    
+    console.log(accs)
+    console.log("^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^")
 }
 
 function crossSubjectAccs() {
@@ -200,60 +207,20 @@ function crossSubjectAccs() {
         }
     }
 
-    console.log(accs);
+    console.log("------ CROSS-SESSION ------")
+    console.log("Network: Riemann-CiM-HRR with DRO")
+    console.log("Mode: Without Riemannian transfer adaption")
+    const precision = 100
+    console.log("Test accuracy without DRO: " + Math.round(accs["testAccBefore"] * 100 * precision ) / precision + "%")
+    console.log("Test accuracy with DRO: " + Math.round(accs["finetunedTestingAcc"] * 100 * precision ) / precision + "%")
+    console.log("^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^")
 
 }
 
-function crossSubjectAccsPercentiles() {
-    const d1 = require("./dimRankingSubjectTransfer_perrcentilesGridSearch_1657563416730.json")
 
-    const run_prefix = "run_"
-    const runs = arange(0, 8)
-
-    const subj_prefix = "subj_"
-    const subjects = arange(1, 10);
-
-    const sessions = ["isReversed_false", "isReversed_true"]
-
-    const percentile_prefix = "percentile_"
-    const percentiles = [0.05, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 0.95]
-
-    const ids = ["trainingAccBefore",
-        "trainingAccAfter",
-        "testAccBefore",
-        "testAccAfter",
-        "finetunedTestingAcc",
-        "finetunedTrainingAcc"
-    ]
-
-    const accs = {}
-    accs["acc_before"] = {}
-    const accsBefore = {}
-    for (const id of percentiles) {
-        accs[percentile_prefix + id] = 0;
-        accs["acc_before"][percentile_prefix + id] = 0
-    }
-    var refAcc = 0
-
-    for (const run of runs) {
-        for (const subj of subjects) {
-            for (const session of sessions) {
-                for (const percentile of percentiles) {
-                    accs[percentile_prefix + percentile] += d1[run_prefix + run][session][subj_prefix + subj][percentile_prefix + percentile]["finetunedTestingAcc"] / (runs.length * sessions.length * subjects.length);
-                    accs["acc_before"][percentile_prefix + percentile] += d1[run_prefix + run][session][subj_prefix + subj][percentile_prefix + percentile]["testAccBefore"] / (runs.length * sessions.length * subjects.length);
-                }
-            }
-        }
-    }
-
-    console.log(accs);
-
-
-}
 
 crossSubjectCrossSessionAccs()
 overfitReduce()
 
 crossSubjectCrossSessionAccsPercentiles()
 crossSubjectAccs()
-crossSubjectAccsPercentiles()
